5fe259c161132f9187eef529065c5de12fdc9cb6
Hanson Lee <leehanson@alumni.ubc.ca>
2022-09-21 02:48:34 +0000

    Function that compares two arrays and returns true if theyre identical

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log( `✅ Assertion Passed: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  let isSame = true;
+  if (array1.length === array2.length) {
+    for (let x = 0; x < array1.length; x++) {
+      if (array1[x] !== array2[x]) {
+        isSame = false;
+      }
+    }
+  }
+  return isSame;
+} 
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
