6b254e2d3e99a5a194d05b138890f4f9c153fcbf
Hanson Lee <leehanson@alumni.ubc.ca>
2022-09-21 23:37:38 +0000

    function that takes a string and returns object with counts of each letter's appearance in string (case sensitive)

---
+++ countLetters.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let countLetter = {};
+  for (let letter of sentence) {
+    if (letter !== " ") {
+      if (countLetter[letter]) {
+        countLetter[letter] += 1;
+      } else {
+        countLetter[letter] = 1;
+      }
+    }
+  }
+  return countLetter;
+};
+
+
+const result1 = countLetters("LHL");
+assertEqual(result1["L"], 2);
+assertEqual(result1["H"], 1);
+
+const result2 = countLetters("Who knows ");
+assertEqual(result2["W"], 1);
+assertEqual(result2["w"], 1);
+
+console.log(result2);
