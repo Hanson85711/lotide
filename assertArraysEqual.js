963f530a1ab4896514c1e555e2c50aa5d9e6f744
Hanson Lee <leehanson@alumni.ubc.ca>
2022-09-21 02:55:56 +0000

    Created function that asserts Array equality

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const eqArrays = function(array1, array2) {
+  let isSame = true;
+  if (array1.length === array2.length) {
+    for (let x = 0; x < array1.length; x++) {
+      if (array1[x] !== array2[x]) {
+        isSame = false;
+      }
+    }
+  }
+  return isSame;
+} 
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log( `✅ Assertion Passed: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
+  }
+}
+
+assertArraysEqual([1,2,3], [1,2,3]);
+assertArraysEqual([1,2,3], [1,2,"3"]);
