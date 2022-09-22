099f40ef57d479fd60352e73c1918fabf2e9a3a5
Hanson Lee <leehanson@alumni.ubc.ca>
2022-09-22 00:17:17 +0000

    takes object and value and returns key that contains value

---
+++ findKeyByValue.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log( `✅ Assertion Passed: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  for (let key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+
+const bestNumbers = { 
+  first_Place: 1,
+  second_Place: 2,
+  third_Place:  3
+};
+
+assertEqual(findKeyByValue(bestNumbers, 1), "first_Place");
+assertEqual(findKeyByValue(bestNumbers, 2), "second_Place");
+assertEqual(findKeyByValue(bestNumbers, 3), "third_Place");
+assertEqual(findKeyByValue(bestNumbers, 4), undefined);
