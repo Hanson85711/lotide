f7446d5b3185d682b14f133c5de6d8b0e6f2c87d
Hanson Lee <leehanson@alumni.ubc.ca>
2022-09-26 22:03:55 +0000

    refactored middle.js to export itself and separated tests onto it's own file

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const eqArrays = function(array1, array2) {
-  let isSame = true;
-  if (array1.length === array2.length) {
-    for (let x = 0; x < array1.length; x++) {
-      if (array1[x] !== array2[x]) {
-        isSame = false;
-      }
-    }
-  } else {
-    isSame = false;
-  }
-  return isSame;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅ Assertion Passed: ${[actual]} === ${[expected]}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
-  }
-};
-
 const middle = function(array) {
   let output = [];
   if (array.length === 1 || array.length === 2) { //If array has only 1 or 2 elements
@@ -38,9 +16,6 @@ const middle = function(array) {
   return output;
 };
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
+
+
