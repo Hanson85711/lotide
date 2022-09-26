afdbe2412fcce2517e1ea283ca0e0d002bb2408e
Hanson Lee <leehanson@alumni.ubc.ca>
2022-09-26 21:51:45 +0000

    refactored eqArrays to be exported and separated tests into it's own file

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log( `✅ Assertion Passed: ${[actual]} === ${[expected]}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   let isSame = true;
   if (array1.length === array2.length) {
@@ -20,8 +12,4 @@ const eqArrays = function(array1, array2) {
   return isSame;
 } 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = eqArrays;
