5076aebfee1949783238ffff3123ecdbe751b400
Hanson Lee <leehanson@alumni.ubc.ca>
2022-09-26 21:57:33 +0000

    refactored assertArraysEqual.js to be exported externally and imports eqArrays instead of copying the entire definition. Separated the tests into another file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-const eqArrays = function(array1, array2) {
-  let isSame = true;
-  if (array1.length === array2.length) {
-    for (let x = 0; x < array1.length; x++) {
-      if (array1[x] !== array2[x]) {
-        isSame = false;
-      }
-    }
-  } else {
-    isSame = false;
-  }
-  return isSame;
-} 
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -20,5 +8,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 }
 
-assertArraysEqual([1,2,3], [1,2,3]);
-assertArraysEqual([1,2,3], [1,2,"3"]);
+module.exports = assertArraysEqual;
